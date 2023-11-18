95ef9a65a2f135ef2118609a2b0f48fc9b0c65e7
AdamShannag <shannagadam11@gmail.com>
2023-11-18 15:01:22 +0300

    fix: types

--- pkg/drivers/oracle/kine/kine.go
+++ pkg/drivers/oracle/kine/kine.go
@@ -25,13 +25,13 @@ func ErrorIs(err error, code OraErrorCode) bool {
 }
 
 type Kine struct {
-	ID             int64  `gorm:"primary_key;type:INTEGER not null"`
+	ID             int64  `gorm:"primary_key;type:NUMBER(10) not null"`
 	Name           string `gorm:"type:VARCHAR2(630)"`
-	Created        int64  `gorm:"type:INTEGER"`
-	Deleted        int64  `gorm:"type:INTEGER"`
-	CreateRevision int64  `gorm:"type:INTEGER"`
-	PrevRevision   int64  `gorm:"type:INTEGER"`
-	Lease          int64  `gorm:"type:INTEGER"`
+	Created        int64  `gorm:"type:NUMBER(10)"`
+	Deleted        int64  `gorm:"type:NUMBER(10)"`
+	CreateRevision int64  `gorm:"type:NUMBER(10)"`
+	PrevRevision   int64  `gorm:"type:NUMBER(10)"`
+	Lease          int64  `gorm:"type:NUMBER(10)"`
 	Value          []byte `gorm:"type:BLOB;create:false"`
 	OldValue       []byte `gorm:"type:BLOB;create:false"`
 }
